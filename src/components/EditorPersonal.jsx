dab9111020a9a1dba42a95eb602abc6ebc28e142
3psilon0 <bestofluck110@gmail.com>
2026-03-29 11:27:31 +0500

    Changed input color

--- src/components/EditorPersonal.jsx
+++ src/components/EditorPersonal.jsx
@@ -14,7 +14,7 @@ function EditorPersonal({name, phone, email, profession, location, aboutme, upda
             <label htmlFor="name" className="flex flex-col">
                 <span className="text-slate-200">Name</span>
                 <input
-                    className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
                     type="text"
                     name="name"
                     id="name"
@@ -26,7 +26,7 @@ function EditorPersonal({name, phone, email, profession, location, aboutme, upda
             <label htmlFor="phone" className="flex flex-col">
                 <span className="text-slate-200">Phone</span>
                 <input
-                    className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
                     type="tel"
                     name="phone"
                     id="phone"
@@ -38,7 +38,7 @@ function EditorPersonal({name, phone, email, profession, location, aboutme, upda
             <label htmlFor="email" className="flex flex-col">
                 <span className="text-slate-200">Email</span>
                 <input
-                    className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
                     type="email"
                     name="email"
                     id="email"
@@ -50,7 +50,7 @@ function EditorPersonal({name, phone, email, profession, location, aboutme, upda
             <label htmlFor="profession" className="flex flex-col">
                 <span className="text-slate-200">Profession</span>
                 <input
-                    className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
                     type="text"
                     name="profession"
                     id="profession"
@@ -62,7 +62,7 @@ function EditorPersonal({name, phone, email, profession, location, aboutme, upda
             <label htmlFor="location" className="flex flex-col">
                 <span className="text-slate-200">Location</span>
                 <input
-                    className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
                     type="text"
                     name="location"
                     id="location"
@@ -74,7 +74,7 @@ function EditorPersonal({name, phone, email, profession, location, aboutme, upda
             <label htmlFor="aboutme" className="flex flex-col">
                 <span className="text-slate-200">About Me</span>
                 <textarea
-                    className="bg-sky-800/40 rounded-lg px-2 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-sky-500/40 focus:border-blue-900"
+                    className="bg-[#1b4576]/80 rounded-lg px-2 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-sky-500/40 focus:border-blue-900"
                     rows={5}
                     name="aboutme"
                     id="aboutme"
