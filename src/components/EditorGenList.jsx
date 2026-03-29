72475d76bc58d8e8f17b0e69b4bd9625ef74bd4c
3psilon0 <bestofluck110@gmail.com>
2026-03-30 03:23:03 +0500

    Added button add new items to general list

--- src/components/EditorGenList.jsx
+++ src/components/EditorGenList.jsx
@@ -1,174 +1,182 @@
-    import { useState } from "react";
-    import { FaPen, FaTrash } from "react-icons/fa";
-    import "../index.css";
+import { useState } from "react";
+import { FaPen, FaTrash, FaPlus } from "react-icons/fa";
+import "../index.css";
 
-    function ItemEditor({ item, handleChange, handleClose }) {
-        return (
-            <div className="flex flex-col gap-4 p-4 px-4 mt-5">
-                <label htmlFor="title" className="flex flex-col">
-                    <span className="text-slate-200">Title</span>
+function ItemEditor({ item, handleChange, handleClose }) {
+    return (
+        <div className="flex flex-col gap-4 p-4 px-4 mt-5">
+            <label htmlFor="title" className="flex flex-col">
+                <span className="text-slate-200">Title</span>
+                <input
+                    className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    type="text"
+                    name="title"
+                    onChange={handleChange}
+                    value={item.title}
+                />
+            </label>
+
+            <label htmlFor="location" className="flex flex-col">
+                <span className="text-slate-200">Location</span>
+                <input
+                    className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    type="text"
+                    name="location"
+                    onChange={handleChange}
+                    value={item.location}
+                />
+            </label>
+
+            <div className="flex gap-10">
+                <label htmlFor="from" className="flex flex-col flex-1 min-w-0">
+                    <span className="text-slate-200">From</span>
                     <input
-                        className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                        className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900 "
                         type="text"
-                        name="title"
+                        name="from"
                         onChange={handleChange}
-                        value={item.title}
+                        value={item.from}
                     />
                 </label>
 
-                <label htmlFor="location" className="flex flex-col">
-                    <span className="text-slate-200">Location</span>
+                <label htmlFor="to" className="flex flex-col flex-1 min-w-0">
+                    <span className="text-slate-200">To</span>
                     <input
-                        className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                        className="bg-[#1b4576] rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
                         type="text"
-                        name="location"
+                        name="to"
                         onChange={handleChange}
-                        value={item.location}
+                        value={item.to}
                     />
                 </label>
+            </div>
 
-                <div className="flex gap-10">
-                    <label htmlFor="from" className="flex flex-col flex-1 min-w-0">
-                        <span className="text-slate-200">From</span>
-                        <input
-                            className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900 "
-                            type="text"
-                            name="from"
-                            onChange={handleChange}
-                            value={item.from}
-                        />
-                    </label>
+            <label htmlFor="description" className="flex flex-col">
+                <span className="text-slate-200">Description</span>
+                <textarea
+                    className="bg-[#1b4576]/80 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
+                    name="description"
+                    rows={5}
+                    onChange={handleChange}
+                    value={item.description}
+                />
+            </label>
 
-                    <label htmlFor="to" className="flex flex-col flex-1 min-w-0">
-                        <span className="text-slate-200">To</span>
-                        <input
-                            className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
-                            type="text"
-                            name="to"
-                            onChange={handleChange}
-                            value={item.to}
-                        />
-                    </label>
-                </div>
+            <div className="flex justify-center text-xs mt-5">
+                <button
+                    className="border-2 border-slate-700 hover:bg-slate-800 text-slate-400 text-sm px-4 py-2 rounded-lg cursor-pointer transition-all duration-200 active:scale-95 shadow-sm hover:shadow-md"
+                    onClick={handleClose}
+                >
+                    Close
+                </button>
 
-                <label htmlFor="description" className="flex flex-col">
-                    <span className="text-slate-200">Description</span>
-                    <textarea
-                        className="bg-sky-800/40 rounded-lg p-2 mt-2 focus:outline-none focus:ring-1 focus:ring-sky-500/40 focus:border-blue-900"
-                        name="description"
-                        rows={5}
-                        onChange={handleChange}
-                        value={item.description}
-                    />
-                </label>
-
-                <div className="flex justify-center text-xs mt-5">
-                    <button
-                        className="border-2 border-slate-700 hover:bg-slate-800 text-slate-400 px-4 py-2 rounded-lg cursor-pointer transition-all duration-200 active:scale-95 shadow-sm hover:shadow-md"
-                        onClick={handleClose}
-                    >
-                        Close
-                    </button>
-
-                    {/* <button className="border border-red-400 text-red-400 hover:bg-red-200/10 px-4 py-2 rounded-lg cursor-pointer transition-all duration-200 active:scale-95" onClick={handleCancel}>
+                {/* <button className="border border-red-400 text-red-400 hover:bg-red-200/10 px-4 py-2 rounded-lg cursor-pointer transition-all duration-200 active:scale-95" onClick={handleCancel}>
                         Cancel
                     </button> */}
-                </div>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
-    function ItemContainer({sectionName, itemList, handleAdd, handleEdit, handleDelete }) {
-        const itemCards = itemList.map((item) => {
-            return (
-                <div
-                    className="flex items-center bg-sky-800/40 rounded-xl border-l-8 border-sky-700 p-4"
-                    key={item.id}
-                    data-item-id={item.id}
-                    onClick={(event) => {
-                        const button = event.target.closest("button");
-
-                        if(!button) return
-
-                        switch(button.dataset.buttonType) {
-                            case 'ListEdit':
-                                handleEdit(event.currentTarget.dataset.itemId);
-                                break
-                            case 'ListDel':
-                                handleDelete(sectionName, event.currentTarget.dataset.itemId);
-                                break
-                        }
-                    }}
-                >
-                    <div className="grow flex flex-col  gap-2">
-                        <h1 className="text-lg font-bold">{item.title}</h1>
-                        <h2 className="text-slate-300 ">{item.location}</h2>
-                        <h3 className="text-slate-400 text-xs italic">
-                            {item.from} - {item.to}
-                        </h3>
-                    </div>
-
-                    <div className="flex gap-5">
-                        <button
-                            className="opacity-70 hover:opacity-100 cursor-pointer"
-                            data-button-type={"ListEdit"}
-                        >
-                            <FaPen size={"1.1rem"} />
-                        </button>
-                        <button
-                            className="opacity-70 hover:opacity-100 cursor-pointer"
-                            data-button-type={"ListDel"}
-                        >
-                            <FaTrash size={"1.1rem"} />
-                        </button>
-                    </div>
-                </div>
-            );
-        });
-
+function ItemContainer({ sectionName, itemList, handleAdd, handleEdit, handleDelete }) {
+    const itemCards = itemList.map((item) => {
         return (
-            <div className="flex flex-col gap-4 mt-5 p-4">
-                {itemCards}
+            <div
+                className="flex items-center bg-sky-800/40 rounded-xl border-l-8 border-sky-700 p-4"
+                key={item.id}
+                data-item-id={item.id}
+                onClick={(event) => {
+                    const button = event.target.closest("button");
+
+                    if (!button) return;
+
+                    switch (button.dataset.buttonType) {
+                        case "ListEdit":
+                            handleEdit(event.currentTarget.dataset.itemId);
+                            break;
+                        case "ListDel":
+                            handleDelete(sectionName, event.currentTarget.dataset.itemId);
+                            break;
+                    }
+                }}
+            >
+                <div className="grow flex flex-col  gap-2">
+                    <h1 className="text-lg font-bold">{item.title}</h1>
+                    <h2 className="text-slate-300 ">{item.location}</h2>
+                    <h3 className="text-slate-400 text-xs italic">
+                        {item.from} - {item.to}
+                    </h3>
+                </div>
+
+                <div className="flex flex-col gap-6">
+                    <button
+                        className="opacity-70 hover:opacity-100 cursor-pointer"
+                        data-button-type={"ListEdit"}
+                    >
+                        <FaPen size={"1.1rem"} />
+                    </button>
+                    <button
+                        className="opacity-70 hover:opacity-100 cursor-pointer"
+                        data-button-type={"ListDel"}
+                    >
+                        <FaTrash size={"1.1rem"} />
+                    </button>
+                </div>
             </div>
         );
-    }
+    });
 
-    function EditorGenList({ sectionName, sectionData, actions }) {
-        const [idToBeEdited, setidToBeEdited] = useState(null);
+    return (
+        <div className="flex flex-col gap-4 p-4">
+            {itemCards}
+            <div className="flex justify-center mt-5">
+                <button className="flex items-center gap-1 text-white text-sm bg-sky-600/90 hover:bg-sky-500 rounded-md px-4 py-2 cursor-pointer" onClick={handleAdd}>
+                    <FaPlus />
+                    Add
+                </button>
+            </div>
+        </div>
+    );
+}
 
-        const startEdit = (itemId) => {
-            setidToBeEdited(itemId);
-        };
-        const stopEdit = () => {
-            setidToBeEdited(null);
-        };
+function EditorGenList({ sectionName, sectionData, actions }) {
+    const [idToBeEdited, setidToBeEdited] = useState(null);
 
-        const editMode = idToBeEdited !== null;
+    const startEdit = (itemId) => {
+        setidToBeEdited(itemId);
+    };
+    const stopEdit = () => {
+        setidToBeEdited(null);
+    };
 
-        if (!editMode) {
-            return (
-                <ItemContainer
+    const editMode = idToBeEdited !== null;
+
+    if (!editMode) {
+        return (
+            <ItemContainer
                 sectionName={sectionName}
                 itemList={sectionData}
-                handleAdd={() => {}}
+                handleAdd={() => {
+                    actions.addGenListItem(sectionName);
+                }}
                 handleEdit={startEdit}
                 handleDelete={actions.delGenListItem}
-                />
-            );
-        } else {
-            return (
-                <ItemEditor
-                    item={sectionData.find(item => item.id === idToBeEdited)}
-                    handleChange={(event) => {
-                        const field = event.currentTarget.getAttribute("name");
-                        const value = event.currentTarget.value;
+            />
+        );
+    } else {
+        return (
+            <ItemEditor
+                item={sectionData.find((item) => item.id === idToBeEdited)}
+                handleChange={(event) => {
+                    const field = event.currentTarget.getAttribute("name");
+                    const value = event.currentTarget.value;
 
-                        actions.updateGenListItem(sectionName, idToBeEdited, field, value);
-                    }}
-                    handleClose={stopEdit}
-                />
-            );
-        }
+                    actions.updateGenListItem(sectionName, idToBeEdited, field, value);
+                }}
+                handleClose={stopEdit}
+            />
+        );
     }
+}
 
-    export default EditorGenList;
+export default EditorGenList;
